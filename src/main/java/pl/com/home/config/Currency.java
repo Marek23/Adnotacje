7cb43522a45647141ffb0e7a361c922869ab4c5f
Marek Szweda <mszweda@kbj.com.pl>
2020-09-17 18:56:35 +0200

    Konfiguracja ResponseBody na xml.
    Z jakiegoś powodu przy adnotacji @XmlRootElement
    potrzebny jest konstruktor nie przyjmujący żadnych
    wartości.

--- src/main/java/pl/com/home/config/Currency.java
+++ src/main/java/pl/com/home/config/Currency.java
@@ -2,7 +2,13 @@ package pl.com.home.config;
 
 import lombok.Data;
 
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
 @Data
+@XmlRootElement
+@NoArgsConstructor
 public class Currency {
 	private String type;
 	private String name;
